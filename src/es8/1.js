eac113efcb467e594428a5ef0828370fd623bc88
Juan Sebastian Rodriguez <juanserodriguezj@gmail.com>
2020-05-23 19:25:24 -0500

    asyn/await

--- src/es8/1.js
+++ src/es8/1.js
@@ -47,3 +47,51 @@ elementos voy a tener osea el maximo de elementos que
 va tener el string y cuales son esos caracteres que voy
 a añadir*/
 console.log(string.padEnd(7, "hi"));
+
+/* ------------- async / await ------------- */
+/* Vamos a hablr de una de las principales funcionalidades
+agregadas en ecmascript8, vamos a entender sus caracteristicas
+y por que es tan aceptada por los desarroladores  */
+
+/* Vamos a crear una funcion despues adentro de ella
+una promesa, depues de la promesa vamos a invocar
+la funcion*/
+// funcion
+const helloWorld = () => {
+  //promesa
+  return new Promise((resolve, reject) => {
+    true //valor a evaluar forzando que paso por el valor positvo
+      ? setTimeout(() => resolve("Hello World"), 3000) //Respuesta afirmativa despues de 3 segundos
+      : reject(new Error("Test Error")); //Respuesta negativa a la promesa
+  });
+};
+/* De esta forma ya tenemos la promesa con la cual
+vamos a trabajar las async/await, esas funciones 
+que la agregan este valor nos indican que van a
+comportarse de forma distinta y nos va permitir a 
+nosotros ser mas claro a la hora de contruirlas y tambien
+vamos a trabajar con el asyncronismo*/
+
+/* Vamos a trabajar con ella y la logica que implica
+usarlas, tenemmos una nueva contante que adentro tendra
+una funcion que empieza con la palabra async y en la
+logica tenemos otra constante que guarda el llamado
+a la funcion creada anteriormente, con await vamos a
+esperar que la promesa termine y luego lo imprime en
+persona  */
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+/* Es la forma correcta para implementar el
+manejo de errores*/
